8b894092f0622715bb8f832092ec8a28dc25cb4d
xwatson <xwatson@foxmail.com>
2018-11-26 18:11:03 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+module.exports = app => {
+  app.appTitle = 'MyTools';
+  app.beforeStart(async () => {
+    // 保证应用启动监听端口前数据已经准备好了
+    // 后续数据的更新由定时任务自动触发
+    if (app.config.env === 'local') {
+        await app.model.sync(); // { force: true }
+    }
+    // await app.runSchedule('update_newMovies');
+  });
+};
