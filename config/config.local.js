b71b25dbe7dbb7a1b16d7a3b8ba93fd736c1521f
xwatson <xwatson@foxmail.com>
2018-11-26 18:12:00 +0800

    add mysql config

---
+++ config/config.local.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.sequelize = {
+    dialect: 'mysql', // support: mysql, mariadb, postgres, mssql
+    database: 'mytools_db',
+    host: 'localhost',
+    port: '3306',
+    username: 'mytools',
+    password: 'xws123123',
+    timezone: '+08:00',
+};
