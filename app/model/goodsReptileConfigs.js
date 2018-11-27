cf917fafdd9eb0e3fa90fc39f89f0a327b65f5e9
xwatson <xwatson@foxmail.com>
2018-11-27 15:53:42 +0800

    跟新model

--- app/model/goodsReptileConfigs.js
+++ app/model/goodsReptileConfigs.js
@@ -12,13 +12,14 @@ module.exports = app => {
       unique: true,
     },
     name: STRING(255), // 名称
+    site_name: STRING(255), // 站点名称
     url: STRING(2000), // 地址
     query_selector: STRING(255), // document选择器
     current_price: DOUBLE, // 现在价格
     lowest_price: DOUBLE, // 最低价格
     expect_price: DOUBLE, // 期望价格
     lowest_price_time: DATE, // 最低价出现时间
-    isPhone: BOOLEAN // 是否移动端
+    is_phone: BOOLEAN // 是否移动端
   });
   return GoodsReptileConfigs;
 };
