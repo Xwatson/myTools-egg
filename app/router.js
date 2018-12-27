33938b7c0d1f733fa7b76b484a6ff76f4b598c16
xwatson <xwatson@foxmail.com>
2018-12-27 16:24:40 +0800

    修改路由

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.post('/api/goodsReptileConfig/get/:id', controller.goodsReptileConfigs.get);
+  router.get('/api/goodsReptileConfig/get/:id', controller.goodsReptileConfigs.get);
   router.post('/api/goodsReptileConfig/create', controller.goodsReptileConfigs.create);
   router.post('/api/goodsReptileConfig/update', controller.goodsReptileConfigs.update);
   router.get('/api/goodsReptileConfig/getList', controller.goodsReptileConfigs.getList);
